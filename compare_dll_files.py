88307fbb5556da9b8c5d9f307b949839ae8fb452
David <davidmacnaugh@gmail.com>
2024-02-13 17:35:49 +1000

    Generalise output/folder arg names.

--- compare_dll_files.py
+++ compare_dll_files.py
@@ -2,7 +2,7 @@ import win32api
 from pathlib import Path
 
 
-def compare(folder_1: Path, folder_2: Path):
+def compare(working_folder: Path, broken_folder: Path):
     """
     To compare the files and dll version numbers of two different directories, simply edit
     the function test_compare() near the bottom of this module, and the run
@@ -12,40 +12,40 @@ def compare(folder_1: Path, folder_2: Path):
     You may need to put in the full path to python.exe, wherever that may be.
 
     """
-    drivers_1 = {path.name for path in folder_1.iterdir() if path.suffix != ".pdb"}
-    drivers_2 = {path.name for path in folder_2.iterdir() if path.suffix != ".pdb"}
-    print("No. dll files found in folder 1: ", len(drivers_1))
-    print("No. dll files found in folder 2: ", len(drivers_2))
+    drivers_in_working1 = {path.name for path in working_folder.iterdir() if path.suffix != ".pdb"}
+    drivers_in_broken2 = {path.name for path in broken_folder.iterdir() if path.suffix != ".pdb"}
+    print("No. dll files found in folder 1: ", len(drivers_in_working1))
+    print("No. dll files found in folder 2: ", len(drivers_in_broken2))
 
-    missing = list(drivers_2 - drivers_1)
-    if len(missing) == 0:
-        print("\nThere are none missing")
+    missing_in_broken2 = list(drivers_in_broken2 - drivers_in_working1)
+    if len(missing_in_broken2) == 0:
+        print("\nThere are no extra dlls in broken folder")
     else:
-        print("Missing from David's")
-    for item in sorted(missing):
+        print("Extra dlls in broken folder (second arg):")
+    for item in sorted(missing_in_broken2):
         print(item)
 
-    extra = list(drivers_1 - drivers_2)
-    if len(extra) == 0:
-        print("\nThere are no extras")
+    extras_in_working1 = list(drivers_in_working1 - drivers_in_broken2)
+    if len(extras_in_working1) == 0:
+        print("\nThere are no extra dll files in the working folder")
     else:
-        print("\n\nExtras in David's")
-    for item in sorted(extra):
+        print("\n\nExtras dll files found in the working (first arg)")
+    for item in sorted(extras_in_working1):
         print(item)
 
-    dll_file_paths_1 = [path for path in folder_1.iterdir() if path.suffix == ".dll"]
-    dll_file_paths_2 = [path for path in folder_2.iterdir() if path.suffix == ".dll"]
-    print("File version metadata for files in David's folder: ")
-    file_versions_david = {get_version(file_path) for file_path in dll_file_paths_1}
-    file_versions_brian = {get_version(file_path) for file_path in dll_file_paths_2}
-    missing_versions = list(file_versions_brian - file_versions_david)
-    print("\n\nMissing versions from David:")
-    for file_version in missing_versions:
+    dll_files_in_working = [path for path in working_folder.iterdir() if path.suffix == ".dll"]
+    dll_files_in_broken = [path for path in broken_folder.iterdir() if path.suffix == ".dll"]
+    print("File version metadata for files in working folder (first arg): ")
+    file_versions_in_working = {get_version(file_path) for file_path in dll_files_in_working}
+    file_versions_in_broken = {get_version(file_path) for file_path in dll_files_in_broken}
+    extra_versions_in_broken = list(file_versions_in_broken - file_versions_in_working)
+    print("\n\nExtra versions in broken folder (second arg):")
+    for file_version in extra_versions_in_broken:
         print(file_version)
 
-    extra_versions = list(file_versions_david - file_versions_brian)
-    print("\n\nExtra versions from David:")
-    for file_version in extra_versions:
+    extra_versions_in_working = list(file_versions_in_working - file_versions_in_broken)
+    print("\n\nExtra versions found in working folder (second arg):")
+    for file_version in extra_versions_in_working:
         print(file_version)
 
 # ver_strings=('Comments','InternalName','ProductName',
@@ -83,9 +83,10 @@ def get_version(fpath: Path):
 
 
 def test_compare():
-    installed_path = Path("C:\\Shepherd Services\\Virtual RACAS 6")
-    debug_path = Path(r"C:\VR_Software_Suite\virtual_racas_npws\virtual-racas\bin\Debug")
-    compare(installed_path, debug_path)
+    broken_folder = Path("C:\\Shepherd Services\\Virtual RACAS 6")
+    # debug_path = Path(r"C:\VR_Software_Suite\virtual_racas_npws\virtual-racas\bin\Debug")
+    working_folder = Path("C:\\Shepherd Services\\Virtual RACAS 6 - NPWS")
+    compare(working_folder, broken_folder)
 
 
 if __name__ == "main":
